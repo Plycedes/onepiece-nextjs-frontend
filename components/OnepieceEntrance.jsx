239ee4891a11e9b5f6b908b3818b4e9c8f484449
Plycedes <abhaypratapyadav340@gmail.com>
2023-09-17 16:17:21 +0530

    Constants and Entrance Function

--- components/OnepieceEntrance.jsx
+++ components/OnepieceEntrance.jsx
@@ -1,3 +1,52 @@
+"use client";
+
+import { useWeb3Contract } from "react-moralis";
+import { abi, localAddress } from "@constants";
+import { useMoralis } from "react-moralis";
+import { useEffect, useState } from "react";
+import { ethers } from "ethers";
+
 export default function OnepieceEntrance() {
-    return <div>Enter</div>;
+    const { isWeb3Enabled } = useMoralis();
+    const [entranceFee, setEntranceFee] = useState("0");
+
+    const { runContractFunction: enterOnePieceRace } = useWeb3Contract({
+        abi: abi,
+        contractAddress: localAddress,
+        functionName: "enterOnePieceRace",
+        params: {},
+        msgValue: entranceFee,
+    });
+
+    const { runContractFunction: getEntranceFee } = useWeb3Contract({
+        abi: abi,
+        contractAddress: localAddress,
+        functionName: "getEntranceFee",
+        params: {},
+    });
+
+    useEffect(() => {
+        if (isWeb3Enabled) {
+            async function updateUI() {
+                const entranceFee = (await getEntranceFee()).toString();
+                setEntranceFee(entranceFee);
+            }
+            updateUI();
+        }
+    }, [isWeb3Enabled]);
+
+    return (
+        <div>
+            <div>
+                <button
+                    onClick={async function () {
+                        await enterOnePieceRace();
+                    }}
+                >
+                    Enter One Piece Race
+                </button>
+            </div>
+            <div>Entrance Fee: {ethers.utils.formatUnits(entranceFee, "ether")} ETH</div>
+        </div>
+    );
 }
